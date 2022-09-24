f7091721587f7b88bbce0b3d6ad15b675114295a
ShreyanshNarang <114290682+ShreyanshNarang@users.noreply.github.com>
2022-09-24 16:50:48 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "ec2_instance" {
+  ami             = "ami-05c8ca4485f8b138a"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.devops_SG.name]
+  key_name        = "aws-prac"
+  user_data       = file("script.sh")
+  tags = {
+    Name = "devops-tf"
+  }
+}
