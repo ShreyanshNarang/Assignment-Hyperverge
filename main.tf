62f4849dc221933ea94ad5383bd267974b1ea960
ShreyanshNarang <114290682+ShreyanshNarang@users.noreply.github.com>
2022-09-27 15:30:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2_instance" {
   ami             = "ami-05c8ca4485f8b138a"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.devops_SG.name]
-  key_name        = "aws-prac"
+  key_name        = "key-tf"
   user_data       = file("script.sh")
   tags = {
     Name = "devops-tf"
